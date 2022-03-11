0fa4aab618f6faff750036ca4a184bde48271819
mindsgn <seni.tembe@protonmail.com>
2022-03-11 17:14:17 +0200

    installed react-navigation

---
+++ src/style/container.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export const container = StyleSheet.create({
+    defaultContainer:{
+        width: '100%', 
+        height:'100%',
+        backgroundColor: 'black'
+    }
+});
+
+ 

07ba78b09647d133038e98344b34efc0a67edd64
mindsgn <seni.tembe@protonmail.com>
2024-01-19 09:55:01 +0200

    update

--- android/app/src/main/java/mindsgn/studio/orbyt/WalletModule.kt
+++ android/app/src/main/java/mindsgn/studio/orbyt/WalletModule.kt
@@ -3,14 +3,6 @@ import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import org.bitcoinj.core.VersionMessage;
-import org.bitcoinj.crypto.MnemonicCode;
-import org.bitcoinj.utils.ContextPropagatingThreadFactory;
-import org.bitcoinj.utils.Threading;
-import org.bitcoinj.wallet.UnreadableWalletException;
-import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.WalletFiles;
-import org.bitcoinj.wallet.WalletProtobufSerializer;
 
 class WalletModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext){
     // private val bitcoinKit: AbstractKit

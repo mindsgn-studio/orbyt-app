55e5a215d3830c49680d0e3d0ae1be648585e4aa
Sibongiseni <48827267+mindsgn@users.noreply.github.com>
2024-05-24 15:43:41 +0200

    Main (#56)
    * empty template
    
    * update

--- App.tsx
+++ App.tsx
@@ -1,51 +1,8 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Loading, Passcode, Home, Token, Error } from './src/screen';
-import { RealmProvider, WalletProvider, AuthProvider } from './src/context';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { ErrorToast, SuccessToast } from "./src/components"
-const Stack = createNativeStackNavigator();
+import { View } from 'react-native';
 
-function App(): React.JSX.Element {
+export default function App() {
   return (
-    <SafeAreaProvider>
-    <GestureHandlerRootView style={{flex: 1}}>
-    <RealmProvider>
-      <WalletProvider>
-        <AuthProvider>
-          <SafeAreaView style={styles.screen}>
-            <ErrorToast  />
-            <SuccessToast  />
-            <NavigationContainer>
-              <Stack.Navigator
-                screenOptions={{
-                  headerShown: false,
-                }}
-              >
-                <Stack.Screen name="Loading" component={Loading} />
-                <Stack.Screen name="Passcode" component={Passcode} />
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Token" component={Token} />
-                <Stack.Screen name="Error" component={Error} />
-              </Stack.Navigator>
-            </NavigationContainer>
-          </SafeAreaView>
-        </AuthProvider>
-      </WalletProvider>
-    </RealmProvider>
-    </GestureHandlerRootView>
-    </SafeAreaProvider>
+    <View>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-});
-
-export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,6 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'transform-remove-console',
-    '@babel/plugin-proposal-export-namespace-from',
-    'react-native-reanimated/plugin',
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
-    ],
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ts', '.js', '.jsx', '.tsx', '.jsx', '.js', '.json'],
-        alias: {
-          '@app': './src',
-        },
-      },
-    ],
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
